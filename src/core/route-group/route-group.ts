c67cb28f7a96b8c5225fd6598486455fb8bd0a3b
Sergey Volkov <js2me@outlook.com>
2025-04-27 17:42:48 +0300

    fix: typings of route groups

--- src/core/route-group/route-group.ts
+++ src/core/route-group/route-group.ts
@@ -1,5 +1,7 @@
 import { computed, makeObservable, observable } from 'mobx';
 
+import { RouteNavigateParams } from '../route/index.js';
+
 import { RoutesCollection } from './route-group.types.js';
 
 declare const process: { env: { NODE_ENV?: string } };
@@ -16,7 +18,7 @@ export class RouteGroup<TRoutesCollection extends RoutesCollection> {
     return routes.some((route) => route.isOpened);
   }
 
-  open(...args: any[]) {
+  open(params?: any, navigateParams?: RouteNavigateParams) {
     let lastGroupRoute: RouteGroup<any> | undefined;
 
     for (const routeName in this.routes) {
@@ -24,13 +26,13 @@ export class RouteGroup<TRoutesCollection extends RoutesCollection> {
       if (route instanceof RouteGroup) {
         lastGroupRoute = route;
       } else if ('isIndex' in route && route.isIndex) {
-        route.open(...args);
+        route.open(params, navigateParams);
         return;
       }
     }
 
     if (lastGroupRoute) {
-      lastGroupRoute.open();
+      lastGroupRoute.open(params, navigateParams);
     } else if (process.env.NODE_ENV !== 'production') {
       console.warn(
         "RouteGroup doesn't have index route. open() method doesn't work.",
