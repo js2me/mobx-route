e383d7d6b814d19986bccf0c2c40fb19392b413a
Sergey Volkov <js2me@outlook.com>
2025-05-30 07:51:52 +0300

    fix: RouteGroupView not correct work

--- src/react/components/route-group-view.tsx
+++ src/react/components/route-group-view.tsx
@@ -1,4 +1,4 @@
-import { memo } from 'react';
+import { memo, ReactNode } from 'react';
 import { EmptyObject } from 'yummies/utils/types';
 
 import {
@@ -39,33 +39,50 @@ export type RouteGroupViews<TRoutes extends RoutesCollection> =
 export interface RouteGroupViewProps<TRouteGroup extends AnyRouteGroup> {
   group: TRouteGroup;
   views: Partial<RouteGroupViews<TRouteGroup['routes']>>;
+  notOpenedContent?: ReactNode;
 }
 
 const RouteGroupViewBase = <TRouteGroup extends AnyRouteGroup>({
   group,
   views,
+  notOpenedContent,
 }: RouteGroupViewProps<TRouteGroup>) => {
   const viewEntries = Object.entries(views);
 
-  return (
-    <>
-      {viewEntries.map(([routeName, propsOrView]) => {
-        // @ts-expect-error Object.entries is not accept types for arrays
-        const route = group.routes[routeName];
-        const viewProps =
-          propsOrView &&
-          (typeof propsOrView === 'function' ||
-            'contextTypes' in propsOrView ||
-            '$$typeof' in propsOrView)
-            ? {
-                view: propsOrView as any,
-              }
-            : propsOrView;
+  const openedRouteEntry = viewEntries.find(([routeName]) => {
+    // @ts-expect-error Object.entries is not accept types for arrays
+    const route = group.routes[routeName];
+    return route.isOpened;
+  });
+  const openedRouteName = openedRouteEntry?.[0];
+  const openedRoutePropsOrView = openedRouteEntry?.[1];
 
-        return <RouteView key={routeName} route={route} {...viewProps} />;
-      })}
-    </>
-  );
+  const openedRoute: AnyRouteEntity | undefined =
+    openedRouteName && openedRouteName in group.routes
+      ? // @ts-expect-error Object.entries is not accept types for arrays
+        group.routes[openedRouteName]
+      : undefined;
+
+  if (!openedRoute) {
+    return notOpenedContent ?? null;
+  }
+
+  const openedRouteViewProps =
+    openedRoutePropsOrView &&
+    (typeof openedRoutePropsOrView === 'function' ||
+      'contextTypes' in openedRoutePropsOrView ||
+      '$$typeof' in openedRoutePropsOrView)
+      ? {
+          view: openedRoutePropsOrView as any,
+          notOpenedContent,
+        }
+      : {
+          ...openedRoutePropsOrView,
+          notOpenedContent:
+            openedRoutePropsOrView.notOpenedContent ?? notOpenedContent,
+        };
+
+  return <RouteView route={openedRoute} {...openedRouteViewProps} />;
 };
 
 export const RouteGroupView = memo(
